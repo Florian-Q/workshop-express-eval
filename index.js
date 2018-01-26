a00b679f727927353acc0b6a86f09f6ea4136b2e
Unknown <florian-q@protonmail.com>
2018-01-26 15:17:26 +0100

    exercice 2
    avec vérification ;)

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = 3000;
+var port = 3000;
+const arg2 = parseInt(process.argv[2]);
+if(arg2 > 0){
+    port = arg2;
+}
+
 
 // start server here
 app.listen(port)
-console.log(chalk.green('Hello web server'));
+console.log(chalk.green('Hello web server ' + port));
